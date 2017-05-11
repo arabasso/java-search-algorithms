4595a1f3d3b83d03096855bb7f435db28023c056
Raphael Basso <arabasso@host.sk>
2017-05-11 19:06:22 -0300

    Correções no código e melhorias.

--- src/main/java/sk/host/arabasso/Application.java
+++ src/main/java/sk/host/arabasso/Application.java
@@ -1,11 +1,56 @@
 package sk.host.arabasso;
 
+import sk.host.arabasso.search.*;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  * Created by arabasso on 01/10/2016.
  *
  */
 public class Application {
     public static void main(String [] args){
+        InputStream input = ClassLoader.class.getResourceAsStream("/input2");
+        Scanner a = new Scanner(input);
 
+        ArrayList<No> nos = new ArrayList<>();
+
+        int n_nodes = a.nextInt();
+
+        for (int i = 0; i < n_nodes; i++) {
+            No x = new No(i, a.nextInt());
+            int childs = a.nextInt();
+            for (int j = 0; j < childs; j++) {
+                x.childs.add(a.nextInt());
+                x.costs.add(a.nextInt());
+            }
+            nos.add(x);
+        }
+
+        int source = a.nextInt();
+        int destiny = a.nextInt();
+
+        int choice = a.nextInt();
+        int fringe = 2;
+
+        Busca busca;
+
+        switch (choice) {
+            case 1:
+                busca = new BFS(nos);
+                break;
+
+            case 4:
+                busca = new AStar(nos);
+                break;
+
+            default:
+                busca = new HillClimbing(nos);
+                break;
+        }
+
+        busca.solve(source, destiny, fringe);
     }
 }
---
+++ src/main/java/sk/host/arabasso/search/Busca.java
@@ -0,0 +1,9 @@
+package sk.host.arabasso.search;
+
+/**
+ * Created by arabasso on 11/05/2017.
+ *
+ */
+public interface Busca {
+    void solve(int source, int destiny, int show);
+}
